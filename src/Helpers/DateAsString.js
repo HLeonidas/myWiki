d51e96fe12ecc2ca10a0b5b74aaf191db363673c
HLeonidas <leon.hueber@feratel.com>
2022-04-12 22:33:02 +0200

    changed date as string

--- src/Helpers/DateAsString.js
+++ src/Helpers/DateAsString.js
@@ -1,5 +1,6 @@
 function getDateAsString(date) {
     if (!date) return "not defined"
+    if (date instanceof Date) return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear()
     if (!date.seconds) return "wrong object"
     let _date = new Date(date.seconds)
     return _date.getDate() + "." + (_date.getMonth() + 1) + "." + _date.getFullYear()
