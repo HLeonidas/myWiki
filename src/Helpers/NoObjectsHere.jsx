d5a429c3d571ab6f68957a9b08128b358f25ce7f
HLeonidas <leon.hueber@feratel.com>
2022-04-12 21:59:41 +0200

    changes on NoObjectsHere

--- src/Helpers/NoObjectsHere.jsx
+++ src/Helpers/NoObjectsHere.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NoObjectsHere({ Title, msg }) {
     return (
         <div className='ml5'>
-            <h1>{Title || "Wählen Sie ein Topic aus"}</h1>
+            <h1>{Title || "Wählen Sie ein Topic aus!"}</h1>
             <p>{msg}</p>
         </div>
     )
